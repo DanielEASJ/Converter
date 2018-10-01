4de6ecf0ff8d83d22049d7b04e743ae6728ed0a0
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-10-01 12:31:52 +0200

    Oprettet projekt med klasse: Converter

---
+++ ConverterDLL/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConverterDLL
+{
+    public class Converter
+    {
+        public void ToGram()
+        {
+            
+        }
+
+        public void ToOuznes()
+        {
+            //bekræft metode navn er stavet rigtigt
+        }
+    }
+}
