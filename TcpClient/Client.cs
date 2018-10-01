6153c71931e8cda65d77be62ea9fc21acd85fadf
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-10-01 17:54:35 +0200

    added TcpClient

---
+++ TcpClient/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpClient
+{
+    class Client
+    {
+        private const int _port = 80;
+
+        public Client()
+        {
+
+        }
+
+        public void Start()
+        {
+            using (System.Net.Sockets.TcpClient socket = new System.Net.Sockets.TcpClient(IPAddress.Loopback.ToString(), _port))
+            {
+                NetworkStream ns = socket.GetStream();
+
+                using (StreamReader sr = new StreamReader(ns))
+                using (StreamWriter sw = new StreamWriter(ns))
+                {
+                    string incommingText = Console.ReadLine();
+
+                    sw.WriteLine($"input was: {incommingText}");
+                    sw.Flush();
+
+                    string line = sr.ReadLine();
+                    sw.WriteLine(line);
+
+                }
+            }
+        }
+    }
+}
---
+++ TcpClient/ClientProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TcpClient
+{
+    class ClientProgram
+    {
+
+        //hvis problemer opstår kan det skyldes at class navnet er ændret fra "program" til "ClientProgram"
+
+
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Start();
+
+        }
+    }
+}
